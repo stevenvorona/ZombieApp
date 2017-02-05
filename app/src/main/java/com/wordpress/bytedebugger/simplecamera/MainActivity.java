131d134973766989ba9f744e836cf6b1947737ff
stevenvorona <stevenvorona@gmail.com>
2017-02-04 19:31:16 -0500

    m

--- app/src/main/java/com/wordpress/bytedebugger/simplecamera/MainActivity.java
+++ app/src/main/java/com/wordpress/bytedebugger/simplecamera/MainActivity.java
@@ -10,12 +10,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+//commimport android.R;
 
 
 public class MainActivity extends Activity {
     private Camera mCamera = null;
     private CameraView mCameraView = null;
+    private TextView xText, yText, zText;
+    private Sensor MySensor;
+    private SensorManager SM;
+
     //this is a test
+    protected void onCreate2(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create our Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        MySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor Listener
+        SM.registerListener((SensorEventListener) this, MySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Assign TextView
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not in use
+    }
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
